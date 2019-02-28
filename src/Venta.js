cc19437fbb2cac6ee189c25b315c5919fd16f08d
Ronald Escobar <ronaldescobarj@gmail.com>
2019-02-28 01:07:43 -0400

    3er test pasando

--- src/Venta.js
+++ src/Venta.js
@@ -5,15 +5,16 @@ class Venta {
     }
 
     total() {
-        if (this.productos.length > 0)
-            return this.productos[0].cuantoPor();
-        else
-            return 0;
+        let total = 0;
+        for (let producto of this.productos) {
+            total += producto.cuantoPor();
+        }
+        return total;
     }
 
-    agregarProducto(producto) {
-        this.productos.push(producto);
-        console.log(this.productos);
+    agregarProducto(producto, cantidad = 1) {
+        for (let i = 0; i < cantidad; i++)
+            this.productos.push(producto);
     }
 }
 
